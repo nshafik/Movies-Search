2915092335e3b74fa526dd431fe2c064ca24e108
Mohammad <mohdos_1999@hotmail.com>
2021-08-22 15:58:18 +0200

    updated movies

--- src/components/AllMovies.js
+++ src/components/AllMovies.js
@@ -1,6 +1,6 @@
 import { Container } from "@material-ui/core"
 import { useEffect, useState } from "react"
-import { getData } from "../services/api"
+import { getData, updateMovieFav } from "../services/api"
 import { makeStyles } from '@material-ui/core/styles';
 import React from 'react';
 import Card from '@material-ui/core/Card';
@@ -12,70 +12,102 @@ import FavoriteIcon from '@material-ui/icons/Favorite';
 import IconButton from '@material-ui/core/IconButton';
 import ShareIcon from '@material-ui/icons/Share';
 import { red } from '@material-ui/core/colors';
+import { addToFavArray, removeFromFavAray } from "../services/localStorageHelper";
 
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      maxWidth: 345,
-    },
-    media: {
-      height: 0,
-      paddingTop: '56.25%', // 16:9
-    },
-    expand: {
-      transform: 'rotate(0deg)',
-      marginLeft: 'auto',
-      transition: theme.transitions.create('transform', {
-        duration: theme.transitions.duration.shortest,
-      }),
-    },
-    expandOpen: {
-      transform: 'rotate(180deg)',
-    },
-    avatar: {
-      backgroundColor: red[500],
-    },
-  }));
+  root: {
+    maxWidth: 600,
+    alignItems: "center",
+    alignContent: "center",
+    margin: "auto",
+    marginBottom: "10px",
+    marginTop: "10px"
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+}));
 
 const AllMovies = () => {
-    const [pageNum, setPageNum] = useState(0)
-    const [movies, setMovies] = useState([])
-    const classes = useStyles();
-    useEffect(() => {
-        setMovies(getData())
-        // getData()
-        // .then(movies => setMovies(movies))
-    }, [])
-    return (
-        <Container>
-            {movies.map( m => (
-                <Card key={m.id} >
-                 <CardMedia
-                   className={classes.media}
-                   title="Paella dish"
-                   square
-                   image={"https://www.publicdomainpictures.net/pictures/320000/velka/background-image.png"}
-                 />
 
-                 <CardContent>
-                   <Typography variant="body2" color="textSecondary" component="p">
-                     {m.name}
-                   </Typography>
-                 </CardContent>
+  const handleFavouriteTap = (movie) => {
+    // let isFav = false;
+    // if (movies[movie.id].isFav) {
+    //   isFav = true;
+    // }
 
-                 <CardActions disableSpacing>
-                   <IconButton aria-label="add to favorites">
-                     <FavoriteIcon />
-                   </IconButton>
-                   <IconButton aria-label="share">
-                     <ShareIcon />
-                   </IconButton>
-                 </CardActions>
-               </Card>
-            )) }
-        </Container>
-   
-    )
+    // isFav = !isFav;
+    const isFav = !movies[movie.id].isFav;
+    // localStorage.setItem(movie.id, isFav);
+    if (isFav) {
+      addToFavArray(movie.id);
+    }
+    else {
+      removeFromFavAray(movie.id);
+    }
+
+    // console.log(movie)
+    // console.log(`setting is fav of ${movie.id} to ${isFav}`)
+    setMovies(prevMovies => updateMovieFav(prevMovies, movie.id, { isFav: isFav }));
+    // setMovies(prevMovies => prevMovies[movie.id]: {})
+  };
+
+  const [pageNum, setPageNum] = useState(0)
+  const [movies, setMovies] = useState({})
+  const classes = useStyles();
+  useEffect(() => {
+    setMovies(getData())
+    // getData()
+    // .then(movies => setMovies(movies))
+  }, [])
+
+  // localStorage.clear();
+  return (
+    <Container>
+      {Object.values(movies).map(m => (
+        <Card key={m.id} className={classes.root}>
+          <CardMedia
+            className={classes.media}
+            title="Paella dish"
+            square
+            image={"https://www.publicdomainpictures.net/pictures/320000/velka/background-image.png"}
+          />
+
+          <CardContent>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {m.name}
+            </Typography>
+          </CardContent>
+
+          <CardActions disableSpacing>
+            <IconButton aria-label="add to favorites" style={{ color: m.isFav ? "orange" : "grey" }} onClick={(event) => handleFavouriteTap(m)}>
+              <FavoriteIcon />
+            </IconButton>
+            <IconButton aria-label="share">
+              <ShareIcon />
+            </IconButton>
+          </CardActions>
+        </Card>
+      ))}
+    </Container>
+
+  )
 }
 
 export default AllMovies;
+
--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,36 @@
 import { v4 as uuidv4 } from 'uuid';
 
 const api_key = window.env.API_KEY
-console.log(api_key);
-export function getData(){
-    let arr = [];
-    for (let i = 0;i < 20;i++)
-        arr.push({id:uuidv4(), name:`${i}th movie`});
-    return arr
-    return Promise((res, rej) => {
-        setTimeout(() => res(arr), 3000)
-    })
+// console.log(api_key);
+export function getData() {
+    let arr = {};
+    for (let i = 0; i < 20; i++) {
+        const id = uuidv4();
+        let isFav = false;
+        if (localStorage.getItem(id)) {
+            isFav = true;
+        }
+        arr[id] = { id: id, name: `${i}th movie`, isFav: isFav };
+        // arr.push({
+        //     [id]: {
+        //         id: id, name: `${i}th movie`, isFav: isFav
+        //     }
+        // });
+
+        // arr.push({id: id, name:`${i}th movie`, isFav: isFav});
+    }
+
+    return arr;
+    // return Promise((res, rej) => {
+    //     setTimeout(() => res(arr), 3000)
+    // })
 }
+
+export function updateMovieFav(movies, movieId, updatedProps) {
+    let moviesUpd = { ...movies };
+    for (let prop of Object.keys(updatedProps)) {
+        moviesUpd[movieId] = { ...moviesUpd[movieId], [prop]: updatedProps[prop] }
+    }
+    return moviesUpd;
+}
+
